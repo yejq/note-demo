f2b05d6b01fcccc683fe6df98abcc1c919770582
jim ye <yejq@vip.qq.com>
2014-06-30 21:22:32 +0800

    create demo

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 echo off
 xcopy source\images  build\html\images /y/e/Q
-pandoc -s -S --toc --toc-depth=5 -c css/manual.css --template=template/default.html5 -A  template/footer.html source/index.md -o build/html/index.html
+pandoc -s -S --toc --toc-depth=5 -c css/manual.css --template=template/default.html5 -A  template/footer.html source/README.md -o build/html/README.html
 
 echo 如果没报错的话，就表示成功了，亲!
 pause
